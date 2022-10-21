6bcbe7aee1cfd277515ad82dd22ca9320fc7cb0c
xiarui.xr <amy@M1-Mac.local>
2022-10-21 16:37:28 +0800

    update plugin configurations to kotlin version

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.8.0"
+}
+
+group = "com.example"
+version = System.getenv("PLUGIN_VERSION")
+
+repositories {
+    mavenCentral()
+}
+
+sourceSets["main"].java.srcDirs("src/main/gen")
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2021.3.3")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"//System.getenv("JDK_VERSION")
+        targetCompatibility = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("213")
+        untilBuild.set("223.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
