7e150458ae780e9528611c6b09841b8522d4121a
rehan <rehanchalanaprsnl@gmail.com>
2024-09-09 21:38:41 +0530

    feat: set up LSP4IJ support for KCL
    - Initialized KCLLanguageServer and KCLLanguageServerFactory
    - Updated gradle.properties to include com.redhat.devtools.lsp4ij:0.4.0 in platformPlugins
    - Added LSP server dependency and language mapping for KCL in plugin.xml
    
    Signed-off-by: rehan <rehanchalanaprsnl@gmail.com>

---
+++ src/main/java/io/kusionstack/kcl/KCLLanguageServerFactory.java
@@ -0,0 +1,15 @@
+package io.kusionstack.kcl;
+
+import com.intellij.openapi.project.Project;
+import com.redhat.devtools.lsp4ij.LanguageServerFactory;
+import com.redhat.devtools.lsp4ij.server.StreamConnectionProvider;
+import org.jetbrains.annotations.NotNull;
+
+public class KCLLanguageServerFactory implements LanguageServerFactory {
+
+    @Override
+    public @NotNull StreamConnectionProvider createConnectionProvider(@NotNull Project project) {
+        return new KCLLanguageServer();
+    }
+
+}
